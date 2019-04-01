e5db295d0937804850996ce0afa76566277439cb
Kolby Kruger <kolbykruger@Kolbys-MacBook-Pro.local>
2019-04-01 13:42:39 -0400

    added absolute header and adjusted slide

--- js/fonts.js
+++ js/fonts.js
@@ -4,7 +4,7 @@
         sessionStorage.fonts = true;
       },
       google: {
-        families: ['Open+Sans:300,400,400i,500,600,700']
+        families: ['Roboto+Condensed:300,400,400i,500,600,700','Cantata+One']
       },
       timeout: 2000
     };
